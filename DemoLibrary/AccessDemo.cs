c294c06fb6d79e5e261290a75e8ab5183dccc64e
davidcol1 <davidmcolvin@gmail.com>
2019-12-11 13:33:42 -0500

    Initial commit of demo using the 6 different access modifiers

---
+++ DemoLibrary/AccessDemo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+  public class AccessDemo
+  {
+    private void Demo()
+    {
+      PublicDemo();
+      InternalDemo();
+      ProtectedInternalDemo();
+      PrivateProtectedDemo();
+      PrivateDemo();
+    }
+
+    private void PrivateDemo()
+    {
+
+    }
+
+    private protected void PrivateProtectedDemo()
+    {
+        
+    }
+
+    protected void ProtectedDemo()
+    {
+
+    }
+
+    protected internal void ProtectedInternalDemo()
+    {
+
+    }
+
+    internal void InternalDemo()
+    {
+
+    }
+
+    public void PublicDemo()
+    {
+
+    }
+
+  }
+}
